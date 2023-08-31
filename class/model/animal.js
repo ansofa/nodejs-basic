7d455994348e6662c36ad776cf83714dd0de122a
ansofa <amursyidansofa@gmail.com>
2023-08-31 22:17:04 +0700

    latihan nodejs

---
+++ class/model/animal.js
@@ -0,0 +1,38 @@
+const fs = require("node:fs/promises");
+class Animal {
+  constructor(name, type, habitat) {
+    this.name = name;
+    this.type = type;
+    this.habitat = habitat;
+  }
+
+  suaraAnimal() {
+    console.log("ROar");
+  }
+
+  async connect() {
+    const file = await fs.readFile("./database/mamalia.json", { encoding: "utf-8" });
+    return eval(file);
+  }
+
+  async findAll() {
+    try {
+      return await this.connect();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async save(body) {
+    try {
+      const data = await this.connect();
+      data.push(body);
+      await fs.writeFile("./database/mamalia.json",  JSON.stringify(data,null,2));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+class Mamalia extends Animal {}
+module.exports = Mamalia;
